ee1bbb723eb4d8e3ace1438f7e2c3fb7e3d70a5c
yayden jang <gdplayerx46@gmail.com>
2025-07-03 21:20:59 -0400

    plogbost color and lineheight and stuff

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to My Blog</h1>
+    <div style={{backgroundColor: "lime"}}>
+      <h1 style={{ 
+        color: "#D000FF", 
+        textAlign: "right", 
+        letterSpacing: "57px", 
+        fontSize: "101px"
+        }}>Welcome to My Blog</h1>
       <p>This is my first post, and it’s built using React!</p>
-      <PlogBost title="plogbost#1"content="plogbost#1 yessirrrr"/>
-      <PlogBost title="plogbost#2."content="plogbost#2 yessirrrrr"/>
-      <PlogBost title="plogbost#3.."content="plogbost#3 yessirrrrrr"/>
-      <PlogBost title="plogbost#4..."content="plogbost#4 yessirrrrrrr"/>
+      <PlogBost title="plogbost#1"content="plogbost#1 yessirrrr" color="aqua"/>
+      <PlogBost title="plogbost#2."content="plogbost#2 yessirrrrr" color="firebrick"/>
+      <PlogBost title="plogbost#3.."content="plogbost#3 yessirrrrrr"color="#F19233"/>
+      <PlogBost title="plogbost#4..."content="plogbost#4 yessirrrrrrr"color="pink"/>
     </div>
 
   );
@@ -17,7 +22,10 @@ function App() {
 
 function PlogBost(props) {
   return (
-    <div>
+    <div style={{
+      backgroundColor: props.color,
+      lineHeight: "8px"
+      }}>
       <h2>{props.title}</h2>
       <p>{props.content}</p>
     </div>
