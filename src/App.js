08c53f4b320c42f63d840c7c44c844c2d35593ea
yayden jang <gdplayerx46@gmail.com>
2025-07-17 21:36:44 -0400

    change like styling and stuff so it looks 'better'

--- src/App.js
+++ src/App.js
@@ -1,33 +1,75 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  MC_RIDE_BIOGRAPHY
+} from './data/blogContent';
+import {
+  APP_NARRATIVE
+} from './data/blogContent';
 
 function App() {
   return (
     <div style={{backgroundColor: "lime"}}>
       <h1 style={{ 
+        fontFamily: "Comic Relief",
         color: "#D000FF", 
         textAlign: "right", 
-        letterSpacing: "57px", 
+        letterSpacing: "57px",
+        backgroundImage: 'linear-gradient(to right,rgb(255, 0, 0),rgb(211, 179, 0), rgb(36, 218, 12), rgb(78, 0, 223), rgb(182, 0, 228)', 
         fontSize: "101px"
         }}>Welcome to My Blog</h1>
-      <p>This is my first post, and it’s built using React!</p>
-      <PlogBost title="plogbost#1"content="plogbost#1 yessirrrr" color="aqua"/>
-      <PlogBost title="plogbost#2."content="plogbost#2 yessirrrrr" color="firebrick"/>
-      <PlogBost title="plogbost#3.."content="plogbost#3 yessirrrrrr"color="#F19233"/>
-      <PlogBost title="plogbost#4..."content="plogbost#4 yessirrrrrrr"color="pink"/>
+      <p>Content here</p>
+      <PlogBost title="mc ride biography by me :)"content={MC_RIDE_BIOGRAPHY} color="aqua"fooooooooooooooo0o0o0ont="Comic Relief"buttonalert="yayyy u like post"/>
+      <PlogBost title='narrative by me titled "App?"'content={APP_NARRATIVE} color="firebrick"fooooooooooooooo0o0o0ont="comic relief"buttonalert="thank u for like post"/>
+      <PlogBost title="plogbost#3.."content="plogbost#3 yessirrrrrr"color="#F19233"fooooooooooooooo0o0o0ont="redacted"buttonalert="asl;kdjfoiawhef"/>
+      <PlogBost title="plogbost#4..."content="plogbost#4 yessirrrrrrr"color="pink"fooooooooooooooo0o0o0ont="comic relief"buttonalert="you liked plogbost 4"/>
     </div>
 
   );
 }
 
 function PlogBost(props) {
+  const [likes, setLikes] = useState(0); // starting at 0
+  
   return (
     <div style={{
+      width: '1701px',
       backgroundColor: props.color,
-      lineHeight: "8px"
+      border: '4px inset yellow',
+      margin: '14px',
+      paddingTop: '4px',
+      paddingLeft: '74px',
+      paddingRight: '74px',
+      borderRadius: '160px',
+      
       }}>
-      <h2>{props.title}</h2>
-      <p>{props.content}</p>
+      <h2 style={{
+        fontFamily: "Comic Relief"
+      }}>{props.title}</h2>
+      <p style={{ marginBottom: '10px',
+        fontFamily: props.fooooooooooooooo0o0o0ont
+      }}>
+        👍 Likes: {likes}
+      </p>
+      <p style={{
+        fontFamily: props.fooooooooooooooo0o0o0ont
+      }}>{props.content}</p>
+      <button
+        style={{
+        backgroundColor: '#4CAF50',
+        color: 'white',
+        padding: '10px 15px',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer',
+        fontFamily: props.fooooooooooooooo0o0o0ont
+        }}
+        onClick={() => setLikes(likes + 1)}
+      >  
+        Like 👍
+      </button>
+  
     </div>
 
   );
---
+++ src/data/blogContent.js
@@ -0,0 +1,55 @@
+export const MC_RIDE_BIOGRAPHY =
+`Stefan Burnett, or his stage name MC Ride, has done many things, but he is most well known for being the lead singer in the experimental hip hop group known as Death Grips. Early life
+
+MC Ride was born on May 10, 1978 in Sacramento, California. He has a brother who also makes music under the name Swank Daddy. He went to Hampton University and studied visual arts, but later dropped out.
+
+Career
+
+Ride formed a hip hop group called Fyre with his brother and a rapper named Young G. This group ended when his brother got married, and because of that, he wasn’t able to keep performing with the group. Also, he had been pursuing painting as a career too.
+
+In 2010, he and his neighbor Zach Hill formed the group Death Grips with Hill’s friend Andy Morin who produced for the group with him. The group then released their debut self-titled EP in March 2011. A month later, Exmilitary was released. The record received critical acclaim. After that, they signed to Epic Records in 2012 and released their debut album, The Money Store, receiving more critical acclaim.
+
+In 2012, Epic Records wanted to wait until 2013 to release their album, No Love Deep Web. So, they decided to leak the album, and the label dropped them. So they released Government Plates under their own label, Third Worlds Records. After that, they released the first half of their double album, The Powers That B in 2014.
+
+However, on July 2, 2014, they announced that they would be disbanding, posting an image with the announcement written on a napkin on Twitter, announcing that they would be breaking up, but will still release the rest of The Powers That B. They finally released the entirety of The Powers That B in March 31, 2015.
+
+In May 2016, after they allegedly disbanded, they released the album Bottomless Pit. After that, they released their EP Steroids (Crouching Tiger Hidden Gabber Megamix) in 2017. Finally, they released their final album to date, Year of the Snitch, and released their final project to date, Gmail and the Restraining Orders in 2019.
+
+Art
+
+Ride has a very aggressive style of rapping, including powerful, abrasive screams and yells, being described as “a visceral and poetic form of rapping, resembling a blend of hardcore punk and spoken word performance,” as well as “paint-peeling barks and startling yelps.” Also, he creates art as a side career, making portraits that are dark and monochromatic.
+
+Personal Life
+
+Even though Ride raps very aggressively in his songs, he seems very soft-spoken and shy in interviews with him. Also, he has stated that he is a very private individual, stating that, “I have very few people that I call my friends. I'm very distrustful of human beings in general; I'm very distrustful of media. I have no interest in sharing my personal life with the world. Zero.”
+
+In addition, Ride has many various tattoos, most of which have dark and Satanic imagery. Some of which being a “111” on his stomach, skulls, spiders, pentagrams, voodoo symbols, a raven, and the word “Death.”
+
+Source
+
+https://en.wikipedia.org/wiki/MC_Ride`
+
+export const APP_NARRATIVE =
+`It was a normal evening, I just got back from bass practice, and I take a look at my phone to check the time. But I noticed something strange: there was a mysterious new app on my phone.
+
+I did not remember installing this app before, so I curiously opened it up, and on the screen suddenly appeared a timer counting down from 24 hours. I suddenly got really frightened, so I immediately closed out of the app, or at least I tried to. When I tried to swipe the app up, it would not move. Closing my other apps worked, but this one just would not budge. And now, there was the counting down timer stuck in the dynamic island at the top of my phone. So now I was frightened, but I realized that I still have 24 hours until something bad can happen. So, I went to sleep and hoped that I would have some kind of dream that would give me a solution.
+
+I did not have some kind of dream that gave me a solution. It was bad. So now I was left with 15 hours left to come up with what this thing could possibly be, and how to fix this. However, it then suddenly came to me. THIS IS DEFINITELY A BOMB. I might be seriously in danger. But who would possibly do this to me? I pondered for a while, and I suddenly realized. FRANK.
+
+For context, a while ago, all the way back in Elementary school, I had this one bully who was named Frank. He always pushed me onto the floor every Wednesday really hard. And it hurt. So, on the last day of fifth grade, I took a bottle of honey to school and squirted honey all over him and he became super sticky for the rest of the day. He went to a different middle school, so he probably still hated me ever since. Because of this, I guess he installed this onto my phone for revenge.
+
+Now that I know that my phone is probably gonna blow up once it counts down to zero, I decided to spend the rest of my day doing some of the things I’ve always wanted to accomplish because you never know how severe a bomb could be. So, I went over and started as many things left on my bucket list as possible. First, I wanted to skydive. So, I bought a plane from my local plane store, as well as a parachute from the parachute store next door, both costing around 50,000 dollars in total. Just kidding, I don’t have enough money. I just constructed my own parachute out of trash bags and tape.
+
+I went on my neighbor’s trampoline with my parachute, and bounced as high as I possibly could. Then I bounced above the entire trampoline, and gently landed with my parachute, as I slammed into the ground face-first. I guess that counted as skydiving. Check! The next thing on my list was to go to a concert. But there were no concerts today that had any tickets available to buy, so I had to create my own concert. I flicked the light switch in my room on and off rapidly. Then I “danced,” just randomly flailing my arms and legs quickly, blasting music. Good enough for a concert. Check!
+
+The next thing after that was to make my own pizza. I went over to the store, bought some dough, went back home, realized I didn’t have tomato sauce, went back to the store, realized I didn’t have cheese, went back, and realized I didn’t have any toppings or anything, and finally got everything. Now that I had all the supplies I needed, I molded the dough into a circle, poured the entire can of tomato sauce onto the pizza dough, poured the entire bag of cheese and all of the toppings straight onto there as well, and then realized that I did not have a pizza oven. But it’s fine because I have a microwave! I threw it into the microwave for 3 minutes at max heat, and let it cook. When I checked on it when it was done, it was all black. I took it out, almost burned my fingerprints off, and took a bite. It actually wasn’t that bad! Just kidding, it was horrible. I had to vomit it all out. But suddenly, I heard beeping from my phone in my pocket.
+
+I quickly pulled out my phone, checking the dynamic island, and there was the timer, counting down from ten seconds, with the text flashing red and white. This was it. I was absolutely cooked. More cooked than the pizza that I had just burned. I was about to blow up, and I had to think of something fast. Time was ticking, beep, seven seconds left, beep, six, beep, five. I needed to think of a solution. IMMEDIATELY. Two seconds were left, and I just decided to chuck the phone as hard as I could towards the window right next to me, hoping that it would break the glass and go through, but the worst thing happened. It bounced back off the window, back over to right in front of my feet.
+
+I was about to blow up into smithereens, I saw my life flashing before my eyes. And as the timer finally counted down to zero seconds, the timer text became a solid red, and a high pitched tone played. This was truly the end, until green confetti started spontaneously spraying from my phone, up towards me. The confetti covered my face, and it stuck to my face. Astonished, I wiped all of it off my face and looked down at my phone. It said,
+
+“Happy birthday! Sorry I couldn’t make it to celebrate your birthday this year, I have been too busy. So, I installed this app on your phone to count down from yesterday, so that I could still celebrate your birthday without me being there! Happy birthday again, son!
+
+Love, Mom.”
+
+I’ve never hated my mom more in my entire life than at that moment.`
