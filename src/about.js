5c37587d336ee9e9c5611d530c0b297460d21db3
yayden jang <gdplayerx46@gmail.com>
2025-09-25 22:00:06 -0400

    changed the home page to be default and also added an amazing picture to the about page hurrah!!!1

--- src/about.js
+++ src/about.js
@@ -1,13 +1,16 @@
 // About.js
 import React from "react";
+import jaydebphoto from "./jaydeb.png"
+
 
 function About() {
   return (
     <div>
       <h1>about this blog website!1</h1>
-      <p>this blog was made by jadyen wyang using react</p>
+      <p>this blog was made by Jayden Yang using react</p>
       <p>here i publish stuff that i wrote for Angela Luo's class</p>
       <p>enjoy m,y mastepprieces i geuses</p>
+      <img src={jaydebphoto}width="1200"height="470"/>
     </div>
   );
 }
